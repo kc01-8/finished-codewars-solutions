49dd715eece08ad5fd18da007df36ea9eb3f8bc4
kc01-8 <minecraftandcallofduty101@gmail.com>
2023-08-08 23:42:59 +0300

    Create bmi.c

---
+++ bmi.c
@@ -0,0 +1,11 @@
+/*    N.B. do not allocate memory,
+   instead: return a string literal  */
+
+const char *bmi (int weight, double height)
+{
+
+    if(weight/(height*height)<=18.5) return "Underweight";
+  else if (weight/(height*height) <=25.0) return "Normal";
+  else if (weight/(height*height)<=30.0) return "Overweight";
+  else return "Obese";
+}
