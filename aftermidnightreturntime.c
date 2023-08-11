ffb36ad23d6ab18660e2b99995aab442f84a88c3
kc01-8 <minecraftandcallofduty101@gmail.com>
2023-08-12 00:04:44 +0300

    Create aftermidnightreturntime.c

---
+++ aftermidnightreturntime.c
@@ -0,0 +1,8 @@
+int past(int hours, int minutes, int seconds) {
+    int totalMilliseconds = 0;
+    totalMilliseconds += hours * 3600000;
+    totalMilliseconds += minutes * 60000;
+    totalMilliseconds += seconds * 1000;
+
+    return totalMilliseconds;
+}
